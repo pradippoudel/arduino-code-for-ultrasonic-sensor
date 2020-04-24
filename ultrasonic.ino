51d7fa2c247a45ceaecab230ec7d1ee9f4f98bf2
Pradip Poudel <50986372+pradippoudel@users.noreply.github.com>
2020-04-24 09:32:34 +0545

    Add files via upload

---
+++ ultrasonic.ino
@@ -0,0 +1,27 @@
+ long dis,dur;
+ void setup()
+ 
+ {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(3,OUTPUT);
+pinMode(4,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(3,0);
+delayMicroseconds(2);
+
+digitalWrite(3,1);
+delayMicroseconds(10);
+
+digitalWrite(3,0);
+dur=pulseIn(4,1);
+
+dis=(0.034/2)*dur;
+
+Serial.println(dis);
+
+
+}
